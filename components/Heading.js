079b97ca3f217611a8c3f28a1e7fc8a27c50e848
Ahtisham-01 <ehtisham.alphasquad@gmail.com>
2023-07-19 15:46:21 +0500

    updated

--- components/Heading.js
+++ components/Heading.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = () => {
     return (
         <div className='w-full max-w-[1280px] mx-auto pt-24 p-4'>
-            <h1 className='text-[40px] font-bold leading-[150%]'>Hi, i'm <span className='text-[#A020F0] text-[3rem] leading-5 font-extrabold'>Ahtisham kiani</span> </h1>
+            <h1 className='text-[40px] font-bold leading-[150%]'>Hi, I'm <span className='text-[#A020F0] text-[2rem] md:text-[3rem] leading-5 font-extrabold'>Ahtisham kiani</span> </h1>
             <p className='pt-4 font-medium text-[18px] w-full max-w-[450px] leading-6'>
                 Software Developer | Mastering React.js | Next.js | React3Fiber | Three.js | 3D Modeling
             </p>
